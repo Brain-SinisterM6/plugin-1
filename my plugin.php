c021c57a452fdcf2ce57848acc86843dc916a9fa
Brain-SinisterM6 <hakimasterk@outlook.com>
2023-11-13 05:03:20 +0100

    first plugin commit

---
+++ my plugin.php
@@ -0,0 +1,10 @@
+<?php
+    /*
+    * Plugin Name: Simple Brain Plugin
+    * Plugin Name:       My Basics Plugin
+    * Description:       This plugin deactivates the admin menu
+    * Version:           1.0.0
+    * Author:            Brain Sinister
+    */
+
+    add_filter( 'show_admin_bar', '__return false');
