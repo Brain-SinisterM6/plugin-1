497c8236017a2f9c87c7181683476984b840c495
Brain-SinisterM6 <hakimasterk@outlook.com>
2023-11-13 05:26:49 +0100

    bug corrected

--- my plugin.php
+++ my plugin.php
@@ -7,4 +7,5 @@
     * Author:            Brain Sinister
     */
 
-    add_filter( 'show_admin_bar', '__return false');
+    /* Disable WordPress Admin Bar for all users */
+    add_filter( 'show_admin_bar', '__return_false' );
